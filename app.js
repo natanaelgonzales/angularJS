16a53816fc1d2915336b493e84bd8b1208104d0a
natanaelgonzales <65197349+natanaelgonzales@users.noreply.github.com>
2021-04-21 20:23:06 -0300

    teste

--- app.js
+++ app.js
@@ -20,15 +20,52 @@ myApp.config(function ($routeProvider) {
     })
 });
 
+myApp.service('nameService', function() {
+    
+    var self = this;
+    this.name = 'John Doe';
+    
+    this.namelenght = function() {
+        
+        return self.name.length;
+        
+    }
+})
+
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
 
-    $scope.name = "Main";
-
+    $scope.person = {
+        name: 'John Doe',
+        address: 'Endereço completo, rua etc'
+    }
+    
 }]);
 
 
-myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
 
     $scope.num = $routeParams.num || 'No param available.';
+    
+    $scope.name = nameService.name;
+    
+    $scope.$watch('name', function() {
+        
+        nameService.name = $scope.name;
+        
+    })
 
 }]);
+
+myApp.directive("searchResult", function() {
+    return {
+        restrict: 'AECM',
+        templateUrl: 'directives/searchresult.html',
+        replace: true,
+        scope: {
+            personName: "@",
+            personAddress: "@" 
+
+            }
+        }
+    
+})
